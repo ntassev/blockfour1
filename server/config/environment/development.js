923976e5c90cdaef539ccf8e648cfd38da61dba7
Nikola Tassev <nikola@cloudsigma.com>
2014-10-15 09:38:23 +0300

    Project skeletion as generated by yeoman angular-fullstack

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/blockfour1-dev'
+  },
+
+  seedDB: true
+};
